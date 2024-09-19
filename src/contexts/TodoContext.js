ac2b4fee1d7ed3dc48f715467ed671a5db7bdc5f
Ajay patel <patel.ajay745@gmail.com>
2024-09-19 06:38:32 -0400

    first commit

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [{ id: 1, todo: "message", compeleted: false }],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const TodoProvider = TodoContext.Provider;
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
